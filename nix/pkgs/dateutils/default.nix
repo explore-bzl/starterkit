3a0e682476177ca6b1fb5f4370b09658f6fbe23c
Aleksander Gondek <gondekaleksander@protonmail.com>
2025-03-16 21:29:20 +0100

    feat(update): update the nixpkgs to latest as of 2025-03-16
    This commit updates the nixpgks definitions to commit
    c80f6a7e10b39afcc1894e02ef785b1ad0b0d7e5 from the nixos-unstable
    branch of the nixpkgs repository.

--- nix/pkgs/dateutils/default.nix
+++ nix/pkgs/dateutils/default.nix
@@ -5,13 +5,13 @@
 }:
 stdenv.mkDerivation {
   name = "ubuntu-lunar-dateutils-amd64";
-  version = "0.4.10-1_amd64";
+  version = "0.4.11-1_amd64";
 
   # Binaries built for standard linux FHS (Ubuntu)
   # that depend solely on glibc
   src = builtins.fetchurl {
-    url = "http://dk.archive.ubuntu.com/ubuntu/pool/universe/d/dateutils/dateutils_0.4.10-1_amd64.deb";
-    sha256 = "3711d7a667b8bd94850e0eb9cb9f124f19cc03ddd4598c743f1cbaa07315a944";
+    url = "http://dk.archive.ubuntu.com/ubuntu/pool/universe/d/dateutils/dateutils_0.4.11-1_amd64.deb";
+    sha256 = "11sdmnw9yrc6dxrvb78vi350v6whixlzvmp164x17xn7gk9f6bh3";
   };
 
   nativeBuildInputs = [gnutar zstd];
